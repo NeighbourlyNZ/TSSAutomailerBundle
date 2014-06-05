584c5f5d3a255a5a6bb0af87507fe26ede90240c
Nathan Kot <me@nathankot.com>
2014-06-05 16:13:02 +1200

    Split up the find next query.

--- Entity/AutomailerRepository.php
+++ Entity/AutomailerRepository.php
@@ -9,24 +9,40 @@ use Doctrine\ORM\EntityRepository;
  */
 class AutomailerRepository extends EntityRepository
 {
+    public function getFindNextQuery($limit)
+    {
+        return $this->getEntityManager()
+                    ->createQuery("
+                        SELECT am
+                        FROM TSSAutomailerBundle:Automailer am
+                        WHERE am.isSent = :is_sent
+                        AND am.isFailed = :is_failed
+                        AND am.isSending = :is_sending
+                        ORDER BY am.createdAt ASC
+                    ")
+                    ->setParameter('is_sent', false)
+                    ->setParameter('is_failed', false)
+                    ->setParameter('is_sending', false)
+                    ->setMaxResults($limit);
+    }
+
     public function findNext($limit)
     {
-        $query = $this->getEntityManager()->createQuery("SELECT am FROM TSSAutomailerBundle:Automailer am WHERE am.isSent = :is_sent AND am.isFailed = :is_failed AND am.isSending = :is_sending ORDER BY am.createdAt ASC")
-				->setParameter('is_sent', false)
-				->setParameter('is_failed', false)
-				->setParameter('is_sending', false)
-				->setMaxResults($limit);
-		return $query->getResult();
+        return $this->getFindNextQuery($limit)->getResult();
     }
-    
+
     public function recoverSending($timeout = 900)
     {
         $timeoutDate = new \DateTime();
         $timeoutDate->modify('-'.$timeout.' seconds');
 
-        $query = $this->getEntityManager()->createQuery("UPDATE TSSAutomailerBundle:Automailer am SET am.isSending = false WHERE am.isSending = true AND am.startedSendingAt <= :timeout_date")
-				->setParameter('timeout_date', $timeoutDate);
+        $query = $this->getEntityManager()->createQuery("
+                    UPDATE TSSAutomailerBundle:Automailer am
+                    SET am.isSending = false 
+                    WHERE am.isSending = true
+                    AND am.startedSendingAt <= :timeout_date
+                 ")->setParameter('timeout_date', $timeoutDate);
 
-		return $query->execute();
+        return $query->execute();
     }
 }
